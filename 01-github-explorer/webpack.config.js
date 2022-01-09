6f1d7162758cd327ddce55991e45bb60c04df145
Felipe Borges <lipex360@gmail.com>
2022-01-09 16:30:36 -0300

    babel import css

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -37,6 +37,11 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader'
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      }
     ]
   }
 }
