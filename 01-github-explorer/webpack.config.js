523b4fc9d05741dedbd1eaac6d5b4076c95004f2
Felipe Borges <lipex360@gmail.com>
2022-01-09 20:34:28 -0300

    start with cra

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(j|t)sx$/,
+        test: /\.tsx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
