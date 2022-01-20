7edfb275ba0c36ead0b373295b950551998ea131
Felipe Borges <lipex360@gmail.com>
2022-01-20 19:49:37 -0300

    header component added

---
+++ 03-ig-news/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import s from './styles.module.scss'
+
+export function Header () {
+  return (
+    <header className={s.container}>
+      <div className={s.content}>
+        <img src="/images/logo.svg" alt="logo ig.news" />
+        <nav>
+          <a className={s.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
