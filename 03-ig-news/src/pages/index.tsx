e45007c310ce0e3bcd52867d9f7d39481f95faae
Felipe Borges <lipex360@gmail.com>
2022-01-20 19:14:29 -0300

    global styles updated

--- 03-ig-news/src/pages/index.tsx
+++ 03-ig-news/src/pages/index.tsx
@@ -1,6 +1,11 @@
-import s from '../styles/home.module.scss'
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <h1 className={s.title}>Hello World</h1>
+    <>
+      <Head><title>Inicio | ig.news</title></Head>
+      
+      <h1>Hello World</h1>
+    </>
   )
 }
