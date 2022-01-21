be45ba20d71bca38a46f532d46fb94764d6a296e
Felipe Borges <lipex360@gmail.com>
2022-01-20 21:53:15 -0300

    Static Site Generator

--- 03-ig-news/src/pages/index.tsx
+++ 03-ig-news/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import Head from 'next/head'
 import { SubscribeButtom } from '../components/SubscribeButtom'
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps:GetServerSideProps = async () => {
+export const getStaticProps:GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KKAbsLj3b5cSTMfgDaBhH5k', {
     expand: ['product']
   })
@@ -52,6 +52,7 @@ export const getServerSideProps:GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 horas = 1 day
   }
 } 
