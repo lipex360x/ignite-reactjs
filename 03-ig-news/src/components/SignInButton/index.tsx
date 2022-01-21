032b7157ab9e41fd269ddb3591e2d9428bfbfa36
Felipe Borges <lipex360@gmail.com>
2022-01-20 22:48:56 -0300

    SignIn OAuth Github

--- 03-ig-news/src/components/SignInButton/index.tsx
+++ 03-ig-news/src/components/SignInButton/index.tsx
@@ -1,12 +1,16 @@
+import { signIn, useSession } from 'next-auth/react'
+
 import { FaGithub } from 'react-icons/fa'
-import {FiX} from 'react-icons/fi'
+import { FiX } from 'react-icons/fi'
+
 import s from './styles.module.scss'
 
 export function SignInButton () {
+  const { data: session } = useSession()
 
-  const isLogin = true;
+  console.log(session)
 
-  return isLogin ? (
+  return session ? (
     <button 
       type='button'
       className={s.button}
@@ -19,6 +23,7 @@ export function SignInButton () {
     <button 
       type='button'
       className={s.button}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417"/>Sign in with GitHub
     </button>
